d8ca7592541b45633ec29666ddeac9721313faff
Natasha <njohnson533@gmail.com>
2021-08-25 15:37:27 -0400

    added minimum.js, takes two arguments and returns the smaller number

---
+++ minimum.js
@@ -0,0 +1,13 @@
+/*
+Write a function min that takes two arguments and returns their minimum.
+*/
+
+function min(a, b) {
+    if (a < b) {
+        return a;
+    }
+    return b;
+}
+
+console.log(min(0, 10));
+console.log(min(0, -10));
